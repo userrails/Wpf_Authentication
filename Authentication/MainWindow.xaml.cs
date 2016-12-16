db121cf2d3dbc5d76deb4a6068f651d82393448d
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-12-16 12:42:58 +0545

    Basic authentication implemented

--- Authentication/MainWindow.xaml.cs
+++ Authentication/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -29,14 +30,55 @@ namespace Authentication
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            string ConString = System.Configuration.ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            string CmdString = String.Empty;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                try
+                {
+                    con.Open();
+                    CmdString = "SELECT * FROM Login where Username='" + txtUsername.Text + "' and Password='" + txtPassword.Text + "' and Role='" + txtRole.Text + "'";
+                    SqlCommand cmd = new SqlCommand(CmdString, con);
+                    //SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    //DataTable dt = new DataTable();
+                    //sda.Fill(dt);
+                    cmd.ExecuteNonQuery();
+                    SqlDataReader dr = cmd.ExecuteReader();
 
+                    int count = 0;
+                    // This checks how many times the record is present.
+                    while (dr.Read())
+                    {
+                        count++;
+                    }
+                    if (count > 1)
+                    {
+                        MessageBox.Show("Username and password are duplicate!!");
+                    }
+                    if (count == 1)
+                    {
+                        MessageBox.Show("You are authenticated!!");
+                    }
+                    if (count < 1)
+                    {
+                        MessageBox.Show("Username or password is not correct, try again!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
-        
     }
 }
